284e60fe70fd44ad3a23a02e484639899dee0f91
mikad74 <mika.doorenbosch@gmail.com>
2022-07-05 16:48:18 +0200

    finished game, could be improved further

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,5 +1,7 @@
+let runningScore = [0,0]
+
 function computerPlays() {
-  let hands = ["Rock", "Paper", "Scissors"];
+  let hands = ["rock", "paper", "scissors"];
   let rngNumber = Math.floor(Math.random() * 3);
 
   return hands[rngNumber];
@@ -11,15 +13,15 @@ function decideWinner(playerChoice, computerChoice) {
   let result = ""
 
   if (playerChoiceConcise == computerChoiceConcise) {
-    result ="It's a tie!";
+    result = ["It's a tie!", 2];
   } else if (playerChoiceConcise == "R" && computerChoiceConcise == "S") {
-    result ="You win!";
+    result = ["You win!", 1];
   } else if (playerChoiceConcise == "P" && computerChoiceConcise == "R") {
-    result ="You win!";
+    result = ["You win!", 1];
   } else if (playerChoiceConcise == "S" && computerChoiceConcise == "P") {
-    result ="You win!";
+    result = ["You win!", 1];
   } else {
-    result ="You loose!";
+    result = ["You loose!", 0];
   }
   return result
 }
@@ -33,13 +35,81 @@ function playAgain(previousResult) {
   }
 }
 
-function playRound() {
+function playRound(playerChoice) {
   let computerChoice = computerPlays();
-  let playerChoice = prompt("Welcome to Rock, Paper, Scissors \nPlease enter your choice: ");
 
   let result = decideWinner(playerChoice, computerChoice);
 
-  playAgain(result);
+  showGame(playerChoice, computerChoice)
+  printResult(result)
+  updateScore(result)
+
 }
 
-playRound();
+function updateScore(result) {
+  const score = document.querySelector(".score")
+  const div = document.createElement("div")
+  runningScore[1 - result[1]]++
+  div.innerText = `${runningScore[0]} - ${runningScore[1]}`
+  score.innerHTML = ""
+  score.appendChild(div)
+  if (runningScore[0] >= 5) {
+    endGame("player")
+  }
+  else if (runningScore[1] >= 5) {
+    endGame("computer")
+  }
+}
+
+
+function endGame(winner) {
+  const p = document.querySelector(".current-game p")
+  p.innerHTML = `<strong>${winner.toUpperCase()}</strong> won the game. \nResetting the score. Click a button to play a new game`
+  p.style.fontSize = "1.2em"
+  console.log(p)
+  runningScore = [0,0]
+}
+
+function initPage() {
+  const currentGame = document.querySelector(".current-game")
+  const p = document.createElement("p")
+  p.innerText = "Welcome to Rock, Paper, Scissors. Please make your choice! First to 5 wins"
+  p.style.fontSize = "1.2em"
+  currentGame.appendChild(p)
+}
+
+function buttonClick(button) {
+  playRound(button.target.className)
+}
+
+function showGame(playerChoice, computerChoice) {
+  const container = document.querySelector(".previous-game")
+  container.innerHTML = ""
+  const playerDiv = document.createElement("div")
+  playerDiv.innerText = "YOU"
+  const computerDiv = document.createElement("div")
+  computerDiv.innerText = "COMPUTER"
+  const playerImg = document.createElement("img")
+  playerImg.src = `../images/${playerChoice}.svg`
+  const div = document.createElement("div")
+  div.innerText = "vs."
+  const computerImg = document.createElement("img")
+  computerImg.src = `../images/${computerChoice}.svg`
+  playerDiv.appendChild(playerImg)
+  container.appendChild(playerDiv)
+  container.appendChild(div)
+  container.appendChild(computerDiv)
+  computerDiv.appendChild(computerImg)
+}
+
+function printResult(result) {
+  const p = document.querySelector(".current-game p")
+  p.innerHTML = `<strong>${result[0]}</strong> Click a button to play again`
+  p.style.fontSize = "1.2em"
+
+}
+
+initPage()
+
+const buttons = document.querySelectorAll("button")
+buttons.forEach(button => button.addEventListener("click", buttonClick))
